cefe3fd86f8a053725a933ee69821e91d573f86d
ogonchar <ostap.gonchar@gmail.com>
2018-05-12 18:29:39 +0200

    Initial

---
+++ src/main/java/com/ostap/weekly/weeklymenu/WeeklyMenuApplication.java
@@ -0,0 +1,12 @@
+package com.ostap.weekly.weeklymenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyMenuApplication.class, args);
+	}
+}
